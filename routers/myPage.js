d6cfecf953192bd0b15aa262b9280af62c595810
sanchoco <jguuun@gmail.com>
2021-04-26 04:18:47 +0900

    카드 데이터 js파일, 마이페이지 틀 작업

--- routers/myPage.js
+++ routers/myPage.js
@@ -1,4 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const multer = require('../lib/multer');
+const { User } = require('../models');
+
+router.patch('/profile', multer.single('profileImg'), async (req, res) => {
+	result = { msg: 'success' };
+	try {
+		if (!req.user || !req.body.nickname) return res.status(400).json({ msg: 'fail' });
+		result['nickname'] = req.body.nickname;
+		result['myIntro'] = req.body.myIntro;
+		result['profileImg'] = req.file.filename;
+
+		console.log(result);
+		res.json({ msg: 'success' });
+	} catch {
+		res.status(400).json({ msg: 'fail' });
+	}
+});
 
 module.exports = router;
