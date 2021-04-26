b52eee79c336ac5cb14bba11b5a9f4b588ad9df6
ehdaydkv112 <ehdaydkv112@naver.com>
2021-04-27 03:43:21 +0900

    bookself정리

--- routers/bookshelf.js
+++ routers/bookshelf.js
@@ -4,22 +4,22 @@ const authMiddleware = require('../auth/authMiddleware')
 const router = express.Router();
 
 // 책장 확인
-router.post('/books/:YYMM', authMiddleware, async (req, res, next) => {
+router.get('/books/:YYMM', authMiddleware, async (req, res, next) => {
     const { YYMM } = req.params
     user = res.locals.user;
-
-    const books = await AnswerCard.find({ _id: user.userId, YYMMDD: { $regex: `${YYMM}..` } })
+    const books = await AnswerCard.find({ userId: user.userId, YYMMDD: { $regex: `${YYMM}..` } })
     return res.send({
         books: books
     })
 });
 
 // 책장 디테일 확인
-router.get('/bookDetail/:YYMMDD', async (req, res, next) => {
+router.get('/bookDetail/:YYMMDD', authMiddleware, async (req, res, next) => {
     const { YYMMDD } = req.params
     user = res.locals.user;
 
-    const booksDetail = await AnswerCard.find({ _id: user.userId, YYMMDD: YYMMDD })
+
+    const boksDetail = await AnswerCard.find({ userId: user.userId, YYMMDD: YYMMDD })
     return res.send({
         booksDetail: booksDetail
     })
