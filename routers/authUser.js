1391d0f35cf58e7e07815a903ec625d483d28b59
cleverlee <dlchdaud1327@gmail.com>
2021-04-26 17:56:24 +0900

    유저정보 수정중

--- routers/authUser.js
+++ routers/authUser.js
@@ -4,7 +4,7 @@ const router = express.Router();
 // 인증 ^^
 router.get("/", (req, res) => {
     console.log(req.user)
-    res.json({user:req.user})
+    res.json({user : req.user})
 })
 
 module.exports = router;
