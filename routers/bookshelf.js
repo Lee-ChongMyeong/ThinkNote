30ed861ecff158fd8de0a3b7c9c8fa84566a4c34
ehdaydkv112 <ehdaydkv112@naver.com>
2021-04-26 17:33:48 +0900

    12

--- routers/bookshelf.js
+++ routers/bookshelf.js
@@ -1,7 +1,18 @@
 const express = require('express');
 const router = express.Router();
 
-
-
+// 책장 확인
+router.post('/books/:YYMM', async (req, res, next) => {
+    const
+    const { YYMM } = req.params
+    try {
+        const result = await QuestionCard.create({
+            contents: req.body['contents'],
+        });
+        res.json({ status: 'success', books: result });
+    } catch (err) {
+        res.json({ status: 'fail' });
+    }
+});
 
 module.exports = router;
