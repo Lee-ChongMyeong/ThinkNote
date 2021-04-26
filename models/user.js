bffc248b5ae1f5329bb784ba5b679c2c8db6738c
sanchoco <jguuun@gmail.com>
2021-04-26 17:47:57 +0900

    유저 모델 버츄얼 수정

--- models/user.js
+++ models/user.js
@@ -14,8 +14,8 @@ user.virtual('userId').get(function () {
 	return this._id.toHexString();
 });
 
-user.virtual('userId').set(function () {
-	return this._id.toHexString();
+user.virtual('_id').set(function () {
+	return this.userId.toHexString();
 });
 
 user.set('toObject', {
