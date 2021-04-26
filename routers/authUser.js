f9caf3ebbf1d73e921aaa2f42a92d5b932cbd6ca
ehdaydkv112 <ehdaydkv112@naver.com>
2021-04-26 17:55:41 +0900

    nickname,profileimg

--- routers/authUser.js
+++ routers/authUser.js
@@ -4,7 +4,10 @@ const router = express.Router();
 // 인증 ^^
 router.get("/", (req, res) => {
     console.log(req.user)
-    res.json({user:req.user})
+    res.json({
+        nickname: req.user.nickname,
+        profileImg: req.user.profileImg
+    })
 })
 
 module.exports = router;
