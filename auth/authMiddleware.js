0a255fe6d4d1b06fcae88806455da725218596d7
ehdaydkv112 <ehdaydkv112@naver.com>
2021-04-27 16:29:20 +0900

    1

--- auth/authMiddleware.js
+++ auth/authMiddleware.js
@@ -3,10 +3,12 @@ const { User } = require('../models')
 require('dotenv').config();
 
 module.exports = (req, res, next) => {
+	console.log('미들웽어 등장')
 	try {
 		const { authorization } = req.headers;
 		const [tokenType, tokenValue] = authorization.split(' ');
-
+		console.log(authorization)
+		console.log(tokenValue)
 		if (tokenType !== 'Bearer') {
 			res.json({
 				msg: 'TypeIncorrect'
@@ -14,7 +16,8 @@ module.exports = (req, res, next) => {
 			return;
 		}
 		const { userId } = jwt.verify(tokenValue, process.env.LOVE_JWT_SECRET);
-
+		console.log('유저아디')
+		console.log(userId)
 		User.findById(userId)
 			.exec()
 			.then((user) => {
