4fcb794ba09615cbcd70857a25a7ad51d32dd1e5
ehdaydkv112 <ehdaydkv112@naver.com>
2021-04-27 13:40:49 +0900

    booksdetail

--- routers/bookshelf.js
+++ routers/bookshelf.js
@@ -20,8 +20,10 @@ router.get('/bookDetail/:YYMMDD', authMiddleware, async (req, res, next) => {
     user = res.locals.user;
 
     const booksDetail = await AnswerCard.findOne({ userId: user.userId, YYMMDD: YYMMDD })
-    const { contents } = await QuestionCard.findOne({ _id: booksDetail.questionId })
-    return res.send({ booksDetail, questionContents: contents })
+    const { contents, createdUser } = await QuestionCard.findOne({ _id: booksDetail.questionId })
+    const { nickname, profileImg } = await User.findOne({ _id: createdUser })
+
+    return res.send({ booksDetail, questionContents: contents, nickname, profileImg })
 });
 
 // 커스텀 질문 등록
