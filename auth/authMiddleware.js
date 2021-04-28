5219c700d23cbd0fce453060fe1e870509039b71
ehdaydkv112 <ehdaydkv112@naver.com>
2021-04-28 16:29:38 +0900

    미들웨어 수정

--- auth/authMiddleware.js
+++ auth/authMiddleware.js
@@ -5,7 +5,6 @@ require('dotenv').config();
 module.exports = (req, res, next) => {
 	try {
 		const { authorization } = req.headers;
-		console.log(authorization)
 		const [tokenType, tokenValue] = authorization.split(' ');
 		if (tokenType !== 'Bearer') {
 			res.json({
