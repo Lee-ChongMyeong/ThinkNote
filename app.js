8181093afb7f766b2ab0bbcd8a78f911b0209531
cleverlee <dlchdaud1327@gmail.com>
2021-04-30 23:06:29 +0900

    머지중

--- app.js
+++ app.js
@@ -26,67 +26,8 @@ moment.tz.setDefault("Asia/Seoul")
 
 
 const http = Server(app)
-<<<<<<< HEAD
-socketIo(http, app)
+//socketIo(http, app)
 
-const io = socketIo(http, {
-	cors: {
-		origin: "*",
-		methods: ["GET", "POST"],
-	},
-})
-
-
-const alarm = io.of("/alarm")
-module.exports = { alarm }
-alarm.on("connection", function (socket) {
-	console.log('New connection')
-	socket.on("joinAlarm", async function (data) {
-		const req = socket.request
-		const { headers: { referer } } = req
-		console.log(referer)
-
-		const { token } = data
-		const { userId } = jwt.verify(token, process.env.LOVE_JWT_SECRET);
-
-		socket.join(userId) // room - 내_id
-		const alarms = await Alarm.find({ userId: userId }).sort({ updatedAt: -1 })
-		msg = []
-		let checked = false
-		for (alarm of alarms) {
-			if (alarm.checked == true)
-				checked = true
-			const temp = {
-				alarmId: alarm._id,
-				userId: alarm.userId,
-				recentNickname: alarm.userList[alarm.userList.length - 1],
-				countOthers: alarm.userList.length - 1,
-				cardId: alarm.cardId,
-				eventType: alarm.eventType,
-				time: calTime(Date(alarm.updatedAt))
-			}
-			msg.push(temp)
-		}
-		alarm.to(userId).emit("joinAlarm", { msg, checked })
-	})
-
-	socket.on("openAlarm", async function (data) {
-		const alarms = await Alarm.updateMany({ _id: socket.room }, { $set: { checked: false } })
-	})
-
-	socket.on("leave", (data) => {
-		console.log("leave")
-		socket.leave(data.room)
-	})
-
-	socket.on("disconnect", () => {
-		console.log("disconnect")
-	})
-})
-=======
-// socketIo(http, app)
-
->>>>>>> 1ae0558b9aba528e1d772a22dd4d30386c612b78
 
 const io = socketIo(http, {
 	cors: {
