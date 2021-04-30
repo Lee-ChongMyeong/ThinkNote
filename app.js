1ae0558b9aba528e1d772a22dd4d30386c612b78
ehdaydkv112 <ehdaydkv112@naver.com>
2021-04-30 22:13:59 +0900

    소켓오류수정

--- app.js
+++ app.js
@@ -25,67 +25,70 @@ require("moment-timezone")
 moment.tz.setDefault("Asia/Seoul")
 
 
-// const http = Server(app)
+const http = Server(app)
 // socketIo(http, app)
 
-// const io = socketIo(http, {
-// 	cors: {
-// 		origin: "*",
-// 		methods: ["GET", "POST"],
-// 	},
-// })
 
+const io = socketIo(http, {
+	cors: {
+		origin: "*",
+		methods: ["GET", "POST"],
+	},
+})
 
-// const alarm = io.of("/alarm")
-// module.exports = { alarm }
-// alarm.on("connection", function (socket) {
-// 	console.log('New connection')
-// 	socket.on("joinAlarm", async function (data) {
-// 		const req = socket.request
-// 		const { headers: { referer } } = req
-// 		console.log(referer)
+const alarm = io.of("/alarm")
 
-// 		const { token } = data
-// 		const { userId } = jwt.verify(token, process.env.LOVE_JWT_SECRET);
+alarm.on("connection", function (socket) {
+	console.log('New connection')
+	socket.on("joinAlarm", async function (data) {
+		const req = socket.request
+		const { headers: { referer } } = req
+		console.log(referer)
+		console.log('==================')
+		console.log('로그인 성공')
 
-// 		socket.join(userId) // room - 내_id
-// 		const alarms = await Alarm.find({ userId: userId }).sort({ updatedAt: -1 })
-// 		msg = []
-// 		let checked = false
-// 		for (alarm of alarms) {
-// 			if (alarm.checked == true)
-// 				checked = true
-// 			const temp = {
-// 				alarmId: alarm._id,
-// 				userId: alarm.userId,
-// 				recentNickname: alarm.userList[alarm.userList.length - 1],
-// 				countOthers: alarm.userList.length - 1,
-// 				cardId: alarm.cardId,
-// 				eventType: alarm.eventType,
-// 				time: calTime(Date(alarm.updatedAt))
-// 			}
-// 			msg.push(temp)
-// 		}
-// 		alarm.to(userId).emit("joinAlarm", { msg, checked })
-// 	})
+		const { token } = data
+		const { userId } = jwt.verify(token, process.env.LOVE_JWT_SECRET);
 
-// 	socket.on("openAlarm", async function (data) {
-// 		const alarms = await Alarm.updateMany({ _id: socket.room }, { $set: { checked: false } })
-// 	})
+		socket.join(userId) // room - 내_id
+		const alarms = await Alarm.find({ userId: userId }).sort({ updatedAt: -1 })
+		msg = []
+		let checked = false
+		for (alarm of alarms) {
+			if (alarm.checked == true)
+				checked = true
+			const temp = {
+				alarmId: alarm._id,
+				userId: alarm.userId,
+				recentNickname: alarm.userList[alarm.userList.length - 1],
+				countOthers: alarm.userList.length - 1,
+				cardId: alarm.cardId,
+				eventType: alarm.eventType,
+				time: calTime(Date(alarm.updatedAt))
+			}
+			msg.push(temp)
+		}
+		alarm.to(userId).emit("joinAlarm", { msg, checked })
+	})
 
-// 	socket.on("leave", (data) => {
-// 		console.log("leave")
-// 		socket.leave(data.room)
-// 	})
+	socket.on("openAlarm", async function (data) {
+		const alarms = await Alarm.updateMany({ _id: socket.room }, { $set: { checked: false } })
+	})
 
-// 	socket.on("disconnect", () => {
-// 		console.log("disconnect")
-// 	})
-// })
+	socket.on("leave", (data) => {
+		console.log("leave")
+		socket.leave(data.room)
+	})
 
+	socket.on("disconnect", () => {
+		console.log("disconnect")
+	})
+})
+
+module.exports = { alarm }
 
 //listen
-app.listen(process.env.LOVE_PORT, () => {
+http.listen(process.env.LOVE_PORT, () => {
 	console.log(`Listening at http://localhost:${process.env.LOVE_PORT}`);
 });
 
