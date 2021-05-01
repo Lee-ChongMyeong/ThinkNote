fd409741781cd59f08cd00ffc63ebb18b9f0fedf
cleverlee <dlchdaud1327@gmail.com>
2021-05-01 16:22:30 +0900

    머지중

--- routers/comment.js
+++ routers/comment.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const sanitize = require('sanitize-html');
-const { CommentBoard, User } = require('../models');
+const { CommentBoard, User, AnswerCard } = require('../models');
 const authMiddleware = require('../auth/authMiddleware')
 const jwt = require('jsonwebtoken')
 const moment = require('moment');
@@ -34,28 +34,24 @@ router.get('/:cardId', async (req, res, next) => {
 
 // 댓글 입력
 router.post('/:cardId', authMiddleware, async (req, res, next) => {
+   const cardId = req.params.cardId;
    const user = res.locals.user;
+   const { userId } = await AnswerCard.findOne({ _id: cardId })
    try {
       let result = {
-         cardId: req.params.cardId,
-         commentContents: sanitize(req.body.commentContents),
-         userId: sanitize(user.id),
-         user: sanitize(user["_id"])
-      }
+				cardId: cardId,
+				commentContents: sanitize(req.body.commentContents),
+				userId: sanitize(user.id),
+			};
+      console.log(result)
       await CommentBoard.create(result);
       result["nickname"] = user.nickname,
       result["profileImg"] = user.profileImg
+      
+      const alarmSend = require('../lib/sendAlarm')
+      await alarmSend(userId, cardId, 'comment', user.userId, req.alarm);
+      
       res.json({ msg: 'success', result: result });
-      const alarm = req.alarm;
-      alarm.to(answer.userId).emit('AlarmEvent', {
-				alarmId: AlarmInfo._id,
-				userId: AlarmInfo.userId,
-				recentNickname: user.nickname,
-				cardId: answerCardId,
-				eventType: 'comment',
-				checked: true,
-				time: AlarmInfo.updatedAt
-			});
 
    } catch (err) {
       console.log(err)
