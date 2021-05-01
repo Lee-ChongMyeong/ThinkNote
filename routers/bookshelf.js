d43073de55d3d90f7c5e08767f170be79e104d28
ehdaydkv112 <ehdaydkv112@naver.com>
2021-05-01 16:53:14 +0900

    e1

--- routers/bookshelf.js
+++ routers/bookshelf.js
@@ -210,11 +210,11 @@ router.post('/question', authMiddleware, async (req, res, next) => {
         const originContents = await QuestionCard.findOne({ contents: contents });
         let { createdAt } = await QuestionCard.findOne({ createdUser: user.userId });
 
-        let createdAt = String.split(' ');
-        console.log(createdAt[1])
-        if (!topic) {
-            return res.send({ msg: '토픽을 넣어주세요' });
-        }
+        // let createdAt = String.split(' ');
+        // console.log(createdAt[1])
+        // if (!topic) {
+        //     return res.send({ msg: '토픽을 넣어주세요' });
+        // }
 
         if (!originContents) {
             const CustomQuestion = await QuestionCard.create({ ...req.body, createdUser: user.userId });
@@ -337,7 +337,6 @@ router.post('/like/answerCard', authMiddleware, async (req, res, next) => {
 
 // 답변카드 좋아요 취소 클릭
 router.patch('/like/answerCard', authMiddleware, async (req, res, next) => {
-<<<<<<< HEAD
     try {
         const { answerCardId } = req.body;
         user = res.locals.user;
@@ -365,35 +364,6 @@ router.patch('/like/answerCard', authMiddleware, async (req, res, next) => {
         console.log(err);
         return res.status(400).json({ msg: 'fail' });
     }
-=======
-	try {
-		const { answerCardId } = req.body;
-		user = res.locals.user;
-
-		const currentLike = await Like.findOne({ userId: user.userId, answerId: answerCardId });
-		if (!currentLike) {
-			return res.send('좋아요가 안되어있는데 어떻게 좋아요를 취소합니까 아시겠어여?');
-		}
-
-		await Like.deleteOne({ answerId: answerCardId, userId: user.userId });
-		let answer = await AnswerCard.findOne({ _id: answerCardId });
-
-		const likeCount = await Like.find({ answerId: answerCardId });
-		const likeCountNum = likeCount.length;
-		let alarmInfo = await Alarm.findOne({ userId: answer.userId, cardId: answerCardId, eventType: 'like' });
-		if (alarmInfo && alarmInfo['userList'].length == 1 && alarmInfo['userList'][0] == user._id) {
-			await Alarm.deleteOne({ userId: answer.userId, cardId: answerCardId, eventType: 'like' });
-		} else {
-			alarmInfo['userList'].splice(alarmInfo['userList'].indexOf(user._id), 1);
-			await alarmInfo.save();
-		}
-		res.send({ answerCardId, likeCountNum, currentLike: false });
-		return;
-	} catch (err) {
-		console.log(err);
-		return res.status(400).json({ msg: 'fail' });
-	}
->>>>>>> 64e06fee81607ada4641b789bcb0726ca32d1a3e
 });
 
 //더보기 질문 타이틀
