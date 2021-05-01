8065670ba16f5ee02bfec5e2ada4266368b28cf4
sanchoco <jguuun@gmail.com>
2021-05-01 18:21:03 +0900

    댓글 삭제 알림 삭제 로직 수정

--- routers/comment.js
+++ routers/comment.js
@@ -1,76 +1,92 @@
 const express = require('express');
 const router = express.Router();
 const sanitize = require('sanitize-html');
-const { CommentBoard, User, AnswerCard } = require('../models');
-const authMiddleware = require('../auth/authMiddleware')
-const jwt = require('jsonwebtoken')
+const { CommentBoard, User, AnswerCard, Alarm } = require('../models');
+const authMiddleware = require('../auth/authMiddleware');
+const jwt = require('jsonwebtoken');
 const moment = require('moment');
 require('moment-timezone');
-moment.tz.setDefault("Asia/Seoul");
+moment.tz.setDefault('Asia/Seoul');
 
 // 댓글 리스트
 router.get('/:cardId', async (req, res, next) => {
-   const cardId = req.params.cardId;
-   let result = { msg: 'success', comments: [] };
-   try {
-      //const comments = await CommentBoard.find({ cardId: cardId }).populate({path:"user"});
-      const comments = await CommentBoard.find({ cardId: cardId }).sort({ date: -1 });
-      for (comment of comments) {
-         const userInfo = await User.findOne({ _id: comment.userId })
-         let temp = {
-            commentId: comment.commentId,
-            commentContents: sanitize(comment.commentContents),
-            userId: comment.userId,
-            nickname: userInfo.nickname,
-            profileImg: userInfo["profileImg"],
-         };
-         result['comments'].push(temp);
-      }
-   } catch (err) {
-      result['msg'] = 'fail';
-   }
-   res.json(result);
+	const cardId = req.params.cardId;
+	let result = { msg: 'success', comments: [] };
+	try {
+		//const comments = await CommentBoard.find({ cardId: cardId }).populate({path:"user"});
+		const comments = await CommentBoard.find({ cardId: cardId }).sort({ date: -1 });
+		for (comment of comments) {
+			const userInfo = await User.findOne({ _id: comment.userId });
+			let temp = {
+				commentId: comment.commentId,
+				commentContents: sanitize(comment.commentContents),
+				userId: comment.userId,
+				nickname: userInfo.nickname,
+				profileImg: userInfo['profileImg']
+			};
+			result['comments'].push(temp);
+		}
+	} catch (err) {
+		result['msg'] = 'fail';
+	}
+	res.json(result);
 });
 
 // 댓글 입력
 router.post('/:cardId', authMiddleware, async (req, res, next) => {
-   const cardId = req.params.cardId;
-   const user = res.locals.user;
-   const { userId } = await AnswerCard.findOne({ _id: cardId })
-   try {
-      let result = {
-				cardId: cardId,
-				commentContents: sanitize(req.body.commentContents),
-				userId: sanitize(user.id),
-			};
-      console.log(result)
-      console.log(await CommentBoard.create(result));
-      result["nickname"] = user.nickname,
-      result["profileImg"] = user.profileImg
-      res.json({ msg: 'success', result: result });
-      
-      const alarmSend = require('../lib/sendAlarm')
-      await alarmSend(userId, cardId, 'comment', user.userId, req.alarm);
-      
+	const cardId = req.params.cardId;
+	const user = res.locals.user;
+	const { userId } = await AnswerCard.findOne({ _id: cardId });
+	try {
+		let result = {
+			cardId: cardId,
+			commentContents: sanitize(req.body.commentContents),
+			userId: sanitize(user.id)
+		};
+		console.log(result);
+		let comment = await CommentBoard.create(result);
+		(result['nickname'] = user.nickname), (result['profileImg'] = user.profileImg);
+		result['commnetId'] = comment._id;
+		res.json({ msg: 'success', result: result });
 
-   } catch (err) {
-      console.log(err)
-      res.json({ msg: 'fail' });
-   }
+		const alarmSend = require('../lib/sendAlarm');
+		await alarmSend(userId, cardId, 'comment', user.userId, req.alarm);
+	} catch (err) {
+		console.log(err);
+		res.json({ msg: 'fail' });
+	}
 });
 
 // 댓글 삭제
 router.delete('/:commentId', authMiddleware, async (req, res, next) => {
-   let result = { msg: 'success' };
-   try {
-      const user = res.locals.user;
-      const commentId = req.params.commentId;
-      const { deletedCount } = await CommentBoard.deleteOne({ _id: commentId, userId: user.id });
-      if (!deletedCount) result['msg'] = 'fail';
-   } catch (err) {
-      result['msg'] = 'fail';
-   }
-   res.json(result);
+	let result = { msg: 'success' };
+	try {
+		console.log(1);
+		const user = res.locals.user;
+		const commentId = req.params.commentId;
+		const commentData = await CommentBoard.findOne({ _id: commentId, userId: user.id });
+      console.log(commentData)
+      const answerCardData = await AnswerCard.findOne({ _id: commentData.cardId });
+      const userId = answerCardData.userId;
+      const answerId = answerCardData._id;
+
+		const { deletedCount } = await CommentBoard.deleteOne({ _id: commentId, userId: user.id });
+		if (!deletedCount) result['msg'] = 'fail';
+
+		let isComment = await CommentBoard.findOne({ cardId: answerId, userId: user.id });
+		let alarmInfo = await Alarm.findOne({ userId: userId, cardId: commentData.cardId, eventType: 'comment' });
+		if (!isComment) {
+			if (-1 != alarmInfo['userList'].indexOf(user._id)) {
+				alarmInfo['userList'].splice(alarmInfo['userList'].indexOf(user._id), 1);
+				await alarmInfo.save();
+			}
+			if (alarmInfo['userList'].length == 0) await Alarm.deleteOne({ userId: userId, cardId: commentData.cardId, eventType: 'comment' });
+		}
+	} catch (err) {
+		console.log(err);
+		result['msg'] = 'fail';
+	}
+	res.json(result);
 });
 
 module.exports = router;
