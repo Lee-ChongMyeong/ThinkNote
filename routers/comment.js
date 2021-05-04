238694ef665d2aa00fcf97ab215c627be5a82682
ehdaydkv112 <ehdaydkv112@naver.com>
2021-05-04 16:01:55 +0900

    태그태그

--- routers/comment.js
+++ routers/comment.js
@@ -20,7 +20,7 @@ router.get('/:cardId', async (req, res, next) => {
 			let temp = {
 				commentId: comment.commentId,
 				commentContents: sanitize(comment.commentContents),
-				commentTag: JSON.stringify(sanitize(comment.tag)),
+				commentTag: sanitize(comment.tag),
 				userId: comment.userId,
 				nickname: userInfo.nickname,
 				profileImg: userInfo['profileImg']
