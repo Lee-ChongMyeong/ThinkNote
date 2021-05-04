e6bfcb0f3a12d8478784db3228597977b00ee295
ehdaydkv112 <ehdaydkv112@naver.com>
2021-05-05 00:49:13 +0900

    푸쉬푸쉬베이베

--- routers/serviceInfo.js
+++ routers/serviceInfo.js
@@ -5,21 +5,45 @@ require('dotenv').config();
 
 //어드민 질문 받아오기
 router.get(`/${process.env.LOVE_SERVICEINFO_QUESTIONLIST}`, async (req, res) => {
-
-    const questionList = await QuestionCard.find({
-        userId: "608971a172444320da6e8671"
-    })
-
+    const questionList = await QuestionCard.find({ createdUser: "608971a172444320da6e8671" })
     adminQuestion = [];
-
     for (let i = 0; i < questionList.length; i++) {
-        console.log(questionList)
-
-        adminQuestion.push(quesitonList.topic)
-        const topic = questionlist.topic
+        const topic = questionList[i]['topic']
+        const contents = questionList[i]['contents']
+        adminQuestion.push(
+            topic + '#' + contents
+        )
+        console.log(topic + '#' + contents)
     }
-
-    res.send(questionlist)
+    return res.send(adminQuestion)
 })
+
+// // 답변 많은순 질문 받아오기
+// router.get(`/test`, async (req, res) => {
+//     const adminId = '608971a172444320da6e8671'
+//     const questionList = await QuestionCard.find({ userId: "608971a172444320da6e8671" })
+//     const popularQuestionList = await QuestionCard.aggregate{ [
+//         { $match: { createdUser: { $eq: adminId } } },
+//         { $project: { _id: { $toString: '$_id', } } }
+//         { $limit: 30 }
+//     ]
+// };
+
+// let adminQuestion = [];
+// for (questionListData of questionList) {
+//     const answerList = await AnswerCard.find({ questionId: questionListData._id })
+//     let answerListCount = answerList.length;
+
+//     adminQuestion.push({
+//         questionId: questionListData._id,
+//     })
+
+//     return res.json(adminQuestion)
+// }
+
+// res.json({ questionlist })
+// })
+
+
 
 module.exports = router;
