9d59d18c7278d5cbf912c2c5955bc67546e64cb3
ehdaydkv112 <ehdaydkv112@naver.com>
2021-05-20 14:46:37 +0900

    router 임시수정

--- routers/index.js
+++ routers/index.js
@@ -15,12 +15,8 @@ router.use('/comment', require('./comment'));
 
 router.use('/serviceInfo', require('./serviceInfo'));
 
-router.use('/friendFeed', require('./friendFeed'));
+// router.use('/friendFeed', require('./friendFeed'));
 
-<<<<<<< HEAD
 // router.use('/topic', require('./topic'));
-=======
-router.use('/topic', require('./topic'));
->>>>>>> 4deeeae2e035938cb77a3844efe32903c0a50107
 
 module.exports = router;
