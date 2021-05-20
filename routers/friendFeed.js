4deeeae2e035938cb77a3844efe32903c0a50107
cleverlee <dlchdaud1327@gmail.com>
2021-05-20 11:56:06 +0900

    friendfeed 코드 작업

--- routers/friendFeed.js
+++ routers/friendFeed.js
@@ -1,15 +1,28 @@
+/* eslint-disable no-unused-vars */
 const express = require('express');
 const router = express.Router();
 const authMiddleware = require('../auth/authMiddleware');
-const { CommentBoard, User, AnswerCard, Alarm } = require('../models');
+const {
+	AnswerCard,
+	User,
+	QuestionCard,
+	Friend,
+	Like,
+	CommentBoard,
+	Search
+} = require('../models');
 const moment = require('moment');
 require('moment-timezone');
 moment.tz.setDefault('Asia/Seoul');
 
-router.get('/', authMiddleware, async(req, res) => {
+router.get('/', authMiddleware, async (req, res) => {
 	let result = { msg: 'success', friendFeed: [] };
 	try {
-		
+		const user = res.locals.user;
+		const myFriend = await Friend.findOne({
+			followingId: user.userId
+		});
+        console.log(myFriend);
 	} catch (err) {
 		result['msg'] = 'fail';
 	}
