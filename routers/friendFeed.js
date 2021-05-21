0c45628b1b09623bb5cd39f3a962999fc72e488f
cleverlee <dlchdaud1327@gmail.com>
2021-05-21 17:38:54 +0900

    친구 피드 무한스크롤 완성

--- routers/friendFeed.js
+++ routers/friendFeed.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
 const express = require('express');
+const mongoose = require('mongoose');
 const router = express.Router();
 const authMiddleware = require('../auth/authMiddleware');
 const { AnswerCard, User, QuestionCard, Friend, Like, CommentBoard, Search } = require('../models');
@@ -13,18 +14,25 @@ router.get('/', authMiddleware, async (req, res) => {
 	let result = { msg: 'success', totalFeed: [] };
 	try {
 		const user = res.locals.user;
-		let friendlist = [];
-		const myFriend = await Friend.aggregate([
-			{ $match: { followingId: user.userId } },
-			{ $project: { followerId: 1 } }
-		]);
+		let lastId = req.query['lastId'];
+		console.log('lastId', lastId);
 
-		for (let friend of myFriend) {
-			friendlist.push(friend.followerId);
-		}
+		// const myFriend = await Friend.aggregate([
+		// 	{ $match: { followingId: user.userId } },
+		// 	{ $project: { followerId: 1 } }
+		// ]);
 
-		const friendCards = await AnswerCard.aggregate([
-			{ $match: { userId: { $in: friendlist } } },
+		// for (let friend of myFriend) {
+		// 	friendlist.push(friend.followerId);
+		// }
+
+		const friendList = await Friend.find({ followingId: user.userId }).then((followers) =>
+			followers.map((follower) => follower.followerId)
+		);
+		console.log(friendList);
+
+		const basicQuery = AnswerCard.aggregate([
+			{ $match: { userId: { $in: friendList } } },
 			{
 				$project: {
 					_id: 1,
@@ -48,19 +56,27 @@ router.get('/', authMiddleware, async (req, res) => {
 					questionId: 1,
 					contents: 1,
 					createdAt: 1,
+					userId: { $arrayElemAt: ['$users._id', 0] },
 					nickname: { $arrayElemAt: ['$users.nickname', 0] },
 					profileImg: { $arrayElemAt: ['$users.profileImg', 0] }
 				}
 			},
-			{ $sort: { createdAt: -1 } },
-			{ $limit: 10 }
+			{ $sort: { createdAt: -1 } }
 		]);
+
+		let friendCards;
+		if (lastId) {
+			friendCards = await basicQuery
+				.match({ _id: { $lt: mongoose.Types.ObjectId(lastId) } })
+				.limit(10);
+		} else {
+			friendCards = await basicQuery.limit(10);
+		}
 
 		for (let i = 0; i < friendCards.length; i++) {
 			let questionInfo = await QuestionCard.findOne({ _id: friendCards[i]['questionId'] });
 			friendCards[i]['questionTitle'] = questionInfo.contents;
 		}
-
 		return res.json({ msg: 'success', friendCards });
 	} catch (err) {
 		return res.json({ msg: 'fail' });
