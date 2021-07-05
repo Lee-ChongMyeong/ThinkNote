845bdacb145602ab259395c16ea1bdb8a71cdad8
cleverlee <dlchdaud1327@gmail.com>
2021-07-05 13:27:02 +0900

    몽구스 버전 업데이트

--- routers/auth.js
+++ routers/auth.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const passport = require('../auth/passport');
 const jwt = require('jsonwebtoken');
+
 router.get('/', (req, res) => {
 	res.send(
 		`<a href='http://lkj99/auth/naver'>네이버 로그인</a> <a href='http://lkj99/auth/google'>구글 로그인</a> <a href='http://lkj99/auth/kakao'> 카카오 로그인 </a>`
